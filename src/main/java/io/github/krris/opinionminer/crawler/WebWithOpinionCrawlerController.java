52ac75de5dd936ba35d83c4c9a4c0b02c6508627
krris <krrisgit@gmail.com>
2014-11-22 15:05:09 +0100

    Bug fix: does not create tmp/ directory.

--- src/main/java/io/github/krris/opinionminer/crawler/WebWithOpinionCrawlerController.java
+++ src/main/java/io/github/krris/opinionminer/crawler/WebWithOpinionCrawlerController.java
@@ -65,7 +65,7 @@ public class WebWithOpinionCrawlerController {
      * You can set the maximum number of pages to crawl. The default value
      * is -1 for unlimited number of pages
      */
-    config.setMaxPagesToFetch(500);
+    config.setMaxPagesToFetch(2000);
 
     /*
      * Do you need to set a proxy? If so, you can use:
